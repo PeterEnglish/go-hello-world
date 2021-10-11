0976aee348bc679b9be48cd8f33b4660516f9535
Peter English <englishpeter29@gmail.com>
2021-10-11 23:07:25 +0100

    Added Eliza loop

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ package main
 //Convention dictates we call the main package 'main'.
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	//Make sure GOPATH is set to the go folder in dev on desktop!
 	//remember to name folder according to whats in .mod file or face errors!
 	"github.com/PeterEnglish/go-hello-world/doctor"
 )
@@ -13,11 +18,27 @@ import (
 //Give package access to code contained in fmt package.
 
 func main() {
+
+	reader := bufio.NewReader(os.Stdin)
 	sayHelloWorld("Hello World!")
 
 	var whatToSay string
 	whatToSay = doctor.Intro()
 	fmt.Println(whatToSay)
+
+	for {
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			response := doctor.Response(userInput)
+			fmt.Println(response)
+		}
+	}
+
 }
 
 func sayHelloWorld(whatToSay string) {
